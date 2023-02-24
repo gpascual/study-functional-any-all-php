66e06eab1b93ac6ee4732efe3a7878872c9ba327
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2023-02-24 20:49:27 +0400

    Add configuration for the email

---
+++ .env.sh
@@ -0,0 +1,4 @@
+## Setting your email for a specific folder (i.e., client-specific)
+export GIT_COMMITTER_EMAIL="alvarogarcia7@users.noreply.github.com"
+export GIT_AUTHOR_EMAIL="$GIT_COMMITTER_EMAIL"
+
